f768daeee33ffb7ca08201df60a73e3942820eb5
Segev BenZvi <segev.benzvi@rochester.edu>
2026-05-05 17:52:36 -0400

    Disable expected failure; add angle-dependent flux check.

--- python/snewpy/test/test_02_models.py
+++ python/snewpy/test/test_02_models.py
@@ -22,6 +22,13 @@ class TestModels(unittest.TestCase):
         self.assertEqual(type(f), flux.Container['1/(MeV*s)'])
         self.assertEqual(len(f.flavor),len(ThreeFlavor))
         self.assertEqual(f[ThreeFlavor.NU_E].unit, 1/(u.MeV * u.s))
+
+    def check_model_spectra_angular(self, model):
+        # Check that we can compute flux containers.
+        f = model.get_initial_spectra([0]*u.s, [10]*u.MeV, theta=0*u.deg, phi=0*u.deg)
+        self.assertEqual(type(f), flux.Container['1/(MeV*s)'])
+        self.assertEqual(len(f.flavor),len(ThreeFlavor))
+        self.assertEqual(f[ThreeFlavor.NU_E].unit, 1/(u.MeV * u.s))
          
     def test_Nakazato_2013(self):
         """
@@ -199,7 +206,6 @@ class TestModels(unittest.TestCase):
 
                 self.check_model_spectra(model)
     
-    @unittest.expectedFailure
     #we know the Fornax_2019 is now inconsistent with our interface
     def test_Fornax_2019(self):
         """
@@ -215,7 +221,7 @@ class TestModels(unittest.TestCase):
             t = model.get_time()
             self.assertTrue(t.unit, u.s)
 
-            self.check_model_spectra(model)
+            self.check_model_spectra_angular(model)
 
     def test_Warren_2020(self):
         """
