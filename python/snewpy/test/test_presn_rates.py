065a70f347a6eef2501ab6791693482f7808db6f
jpkneller <jpknelle@ncsu.edu>
2026-05-29 15:08:03 -0400

    Update test_presn_rates.py
    Move Kato_2017 to a separate test.

--- python/snewpy/test/test_presn_rates.py
+++ python/snewpy/test/test_presn_rates.py
@@ -15,12 +15,21 @@ distance = 200*u.pc
 T = np.geomspace(-1*u.hour, -1*u.min,1000)
 E = np.linspace(0,20,100)*u.MeV
 
-@pytest.mark.parametrize('model_class',[presn.Odrzywolek_2010, presn.Kato_2017, presn.Patton_2017, presn.Yoshida_2016])
+@pytest.mark.parametrize('model_class',[presn.Odrzywolek_2010, presn.Patton_2017, presn.Yoshida_2016])
 @pytest.mark.parametrize('transformation',[AdiabaticMSW(MixingParameters(mh)) for mh in MassHierarchy])
 @pytest.mark.parametrize('detector', ["wc100kt30prct"])
 def test_presn_rate(model_class, transformation, detector):
     model = model_class(progenitor_mass=15*u.Msun)
     flux = model.get_flux(T, E, distance=distance, flavor_xform=transformation)
+    rate = rc.run(flux, detector='scint20kt', detector_effects=False)['ibd']
+    ibd_events = rate.integrate_or_sum('time').integrate_or_sum('energy').array.squeeze()
+    assert 10<ibd_events<1000
+
+@pytest.mark.parametrize('model_class',[presn.Kato_2017])
+@pytest.mark.parametrize('detector', ["wc100kt30prct"])
+def test_presn_rate(model_class, transformation, detector):
+    model = model_class(progenitor_mass=15*u.Msun)
+    flux = model.get_flux(T, E, distance=distance)
     rate = rc.run(flux, detector='scint20kt', detector_effects=False)['ibd']
     ibd_events = rate.integrate_or_sum('time').integrate_or_sum('energy').array.squeeze()
     assert 10<ibd_events<1000
