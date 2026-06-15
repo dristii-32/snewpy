eb2623f2b46dbe893673dad4ea50dc86a99529ae
jpkneller <jpknelle@ncsu.edu>
2026-06-15 12:11:30 -0400

    Update pisn_loaders.py

--- python/snewpy/models/pisn_loaders.py
+++ python/snewpy/models/pisn_loaders.py
@@ -3,11 +3,9 @@
 The submodule ``snewpy.models.pisn_loaders`` contains classes to load pair-instability supernova
 models from files stored on disk.
 """
-import logging
 
 from snewpy.models.base import SNOwGLoBES
 from snewpy import _model_downloader
-
 
 class Wright_2017(SNOwGLoBES):
     """PISN model described in the paper `Neutrino signal from pair-instability supernovae' by Wright et al.,  
