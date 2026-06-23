3ccabd3ef395f3b2c7e06bde7debc828993e9d80
jpkneller <jpknelle@ncsu.edu>
2026-06-23 11:09:44 -0400

    Update utils.py

--- python/snewpy/utils.py
+++ python/snewpy/utils.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 def expand_dimensions_to(a:np.ndarray, ndim:int)->np.ndarray:
@@ -5,3 +6,15 @@ def expand_dimensions_to(a:np.ndarray, ndim:int)->np.ndarray:
     so total dimensions equal to `ndim`"""
     new_shape = (list(a.shape)+[1]*ndim)[:ndim]
     return a.reshape(new_shape)
+    
+def strip_extensions(filename):
+    strip_extensions = ['.dat', '.txt', '.fits', '.h5', '.tar', '.gz', '.bz2']
+    # Split the filename into the base and the extension
+    while True:
+        filename, ext = os.path.splitext(filename)
+        print(filename,ext)
+        if ext.lower() not in strip_extensions:
+            filename += ext
+            break    
+    return filename
+    
