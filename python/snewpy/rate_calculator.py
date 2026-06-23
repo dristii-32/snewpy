db91441c84b3ae752e59977ce2f1f2148049d977
jpkneller <jpknelle@ncsu.edu>
2026-06-23 13:56:28 -0400

    Update rate_calculator.py

--- python/snewpy/rate_calculator.py
+++ python/snewpy/rate_calculator.py
@@ -221,11 +221,13 @@ class DetectionChannel:
             If `True`(default) apply the efficiency, otherwise assume 100% efficiency
         """
         rate = self._calc_interaction_rate(flux)
+        print(rate,"\n")
         if apply_smearing:
             if self.smearing is not None:
                 rate = self.smearing.apply(rate)
         if apply_efficiency:
                 rate = rate*self.efficiency
+        print(rate,"\n")            
         return rate
         
     def _calc_interaction_rate(self, flux):
