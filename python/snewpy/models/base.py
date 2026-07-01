8bf2ace2f18fe315923b47e16f4b2be47caa6dad
jpkneller <jpknelle@ncsu.edu>
2026-07-01 15:12:59 -0400

    Update base.py

--- python/snewpy/models/base.py
+++ python/snewpy/models/base.py
@@ -47,6 +47,8 @@ class SupernovaModel(ABC, LocalFileLoader):
         """
         self.time = time
         self.metadata = metadata
+        for entry in metadata:
+            self.name += f"{metadata[entry]"
         
     def __repr__(self):
         """Default representation of the model.
