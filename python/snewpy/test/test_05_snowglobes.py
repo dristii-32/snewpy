4743f2eed9c5a29052107922a29f51c900e623f9
jpkneller <jpknelle@ncsu.edu>
2026-07-01 21:11:26 -0400

    Update test_05_snowglobes.py

--- python/snewpy/test/test_05_snowglobes.py
+++ python/snewpy/test/test_05_snowglobes.py
@@ -18,24 +18,21 @@ model = ccsn.Bollig_2016
 for params in model.get_param_combinations():
     model(**params)
     
-def fluence_calculation(model_name,model_file,transform):
+def fluence_calculation(model_name,transform):
     #generating fluence file
-    model_file_path = f'{model_path}/{model_name}/{model_file}'
-    print(model_file_path)
-    return snowglobes.generate_fluence(model_file_path, model_name, transform,d=10)
+    return snowglobes.generate(model_name, transform,d=10)
 
-def rates_calculation(fluence_file):
-    tables = snowglobes.simulate(None,fluence_file,detector_input=detectors)
+def rates_calculation(fluence):
+    tables = snowglobes.simulate(None,fluence,detector_input=detectors)
     result = {}
-    for det,table in tables.items():
-        table = list(table.values())[0] #take the first and the only time bin
-        result[det] = table['weighted']['smeared'].values.sum()
+    for det,chan in tables.items():
+        result[det] += table[det][chan].sum().array.squeeze()
     return result
 
 @pytest.mark.parametrize('model_parameters',param_values)
 def test_total_rate_equals_table_value(model_parameters):
-    fluence_file = fluence_calculation(*model_parameters)
-    calculated_rates  = rates_calculation(fluence_file)
+    fluence = fluence_calculation(*model_parameters)
+    calculated_rates  = rates_calculation(fluence)
     for detector in detectors:
         expected = pytest.approx(rate_table[model_parameters][detector], rel=0.01)
         assert calculated_rates[detector] == expected, f"Crosscheck failed for {detector}"
