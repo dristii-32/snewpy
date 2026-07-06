1ae7929325e59dc0fc7efe67b5632637ac214702
jpkneller <jpknelle@ncsu.edu>
2026-07-06 19:29:50 -0400

    Update snowglobes.py

--- python/snewpy/snowglobes.py
+++ python/snewpy/snowglobes.py
@@ -115,7 +115,8 @@ def get_model_loader(model_type: str):
     for module_name in modules_list:
         module = importlib.import_module(module_name)
         models_dict.update({k:v for k,v in vars(module).items() if isclass(v)})
-
+    models_dict['Analytic3Species'] = Analytic3Species
+    
     try:
         return models_dict[model_type]
     except KeyError:
